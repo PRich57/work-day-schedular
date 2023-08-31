baff8d707747a9713c324ca81687d5a5236f8547
PRich57 <pcrichards57@gmail.com>
2023-08-31 15:53:57 -0600

    Add starter code and begin pseudocoding

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Add current day at top of page with dayjs 
+
+// When I view the time blocks the colors indicate past present or future
+
+// Variable that holds current hour
+
+// Make array of time blocks on schedule
+
+// Compare hour variable to time block array
+
+    // if hour is greater than time block 
+        // time block turns gray
+
+    // else if hour === time block
+        // time block turns red
+
+    // else
+        // time block green
+
+// Make each time block an input field with submit button
+
+    // Make submit event that adds input to local storage
+
+// Make function that parses and appends local storage to page
+
+// Local storage 
