1188c70f25f0550b3c4108b699a18275b36045e5
PRich57 <pcrichards57@gmail.com>
2023-08-31 22:46:07 -0600

    Add final comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-// VARIABLE DECLARATIONS
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  
-  // var textareaEl = $('.description');
-  var saveBtn = $('.saveBtn');
+  // VARIABLE DECLARATIONS
+  var saveBtn = $(".saveBtn");
+  // Declare tasks object to store parsed local storage
   var tasks = JSON.parse(localStorage.getItem("tasks")) || {};
-  
+  // Declare today variable to easily use dayjs() for multiple purposes in this code
+  var today = dayjs();
+  // Declare currentDay variable to store a reference to the currentDay id
+  var currentDay = $("#currentDay");
+  // Declare a currentHour variable for easy comparison against div times and parse int to return number
+  var currentHour = parseInt(today.format("H"));
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -18,28 +21,33 @@ $(function () {
   // useful when saving the description in local storage?
   //
 
-  saveBtn.on('click', storeEvents);
-      for (const key in tasks) {
-        var timeBlock = $('#' + key).children()[1];
-        timeBlock.value = tasks[key];
-        console.log(timeBlock);
-      }
-      
-      function storeEvents(event) {
-        var eventStore = {
-          event: $(this).siblings()[1].value,
-          time: $(this).parent().attr("id"),
-        }
-        
-        tasks[eventStore.time] = eventStore.event;
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        
-        console.log(tasks);
-        console.log(eventStore);
-      }
-      
+  // Event listener for click on saveBtn
+  saveBtn.on("click", storeTasks);
+  // for each key in tasks
+  for (const key in tasks) {
+    // Declare timeBlock variable to store the each '#hour-i' element's child at the first index --> textarea
+    var timeBlock = $("#" + key).children()[1];
+    // Take tasks at the key index (user input for each textarea) and assign it as the timeBlock variable's value
+    timeBlock.value = tasks[key];
+  }
 
-      
+  // Create function to store tasks
+  function storeTasks(event) {
+    // Declare object variable with keys of event and time
+    var storeTask = {
+      // Task has a value of the sibling in the 1 index position of the element triggering the event
+      // In other words "this" is the saveBtn (index 2) and the sibling's (index 1) value is the user input of the text area
+      task: $(this).siblings()[1].value,
+      // Time has a value of the saveBtn's parent id, or "#hour-i"
+      time: $(this).parent().attr("id"),
+    };
+
+    // Give the tasks key a value of time/task pairs in local storage to keep tasks associated with their designated time
+    tasks[storeTask.time] = storeTask.task;
+    // Set items in local storage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -52,22 +60,23 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
   // handleSaveButton();
-  
-  var today = dayjs();
 
-  var currentDay = $('#currentDay');
-
+  // Assign text to the currentDay variable in the desired format from dayjs
   currentDay.text(today.format("dddd, MMMM D, YYYY h:mm A"));
-  var currentHour = today.format("H");
 
-  for (var i = 9; i < 18; i++){
-    var timeB = $('#hour-' + i);
+  // for loop using military time to account for rollover from 12 to 1
+  for (var i = 9; i < 18; i++) {
+    // Declare variable relativeTime to compare current hour with hour on the scheduler
+    var relativeTime = $("#hour-" + i);
+    // if current hour is greater than current index div's time then assign that div a class of "past"
     if (i < currentHour) {
-      timeB.addClass("past");
-    } else if (i == currentHour) {
-      timeB.addClass("present");
+      relativeTime.addClass("past");
+    // else if current hour is equal to current index then assign present
+    } else if (i === currentHour) {
+      relativeTime.addClass("present");
+    // else, assign future
     } else {
-      timeB.addClass("future");
+      relativeTime.addClass("future");
     }
   }
 });
